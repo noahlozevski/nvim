45628e3361ce8edcbd87f314c0c8aeaf97a0e2da
noah lozevski <55212226+noahlozevski@users.noreply.github.com>
2023-04-11 10:44:00 -0700

    updated neotree config

--- after/plugin/neo-tree.lua
+++ after/plugin/neo-tree.lua
@@ -146,7 +146,7 @@ require("neo-tree").setup({
     nesting_rules = {},
     filesystem = {
         filtered_items = {
-            visible = false, -- when true, they will just be displayed differently than normal items
+            visible = true, -- when true, they will just be displayed differently than normal items
             hide_dotfiles = true,
             hide_gitignored = true,
             hide_hidden = true, -- only works on Windows for hidden files/directories
