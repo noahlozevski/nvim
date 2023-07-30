6b1b015865e50f7d6346712ffe54606b5cf200d1
noah lozevski <noahlozevski@gmail.com>
2023-07-29 22:25:30 -0700

    better config

--- lua/lozevski/remap.lua
+++ lua/lozevski/remap.lua
@@ -11,6 +11,9 @@ end
 -- no_remap("<Left>", "<Nop>")
 -- no_remap("<Right>", "<Nop>")
 
+no_remap("<C-f>", "<cmd>silent !tmux neww /Users/noahlozevski/.scripts/tmux-sessionizer.sh<CR>")
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
