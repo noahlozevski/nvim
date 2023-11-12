3c69572a9a38439ab2e603079d2b4aad44f691f8
noah lozevski <noahlozevski@gmail.com>
2023-11-12 12:42:09 -0800

    last time

--- after/ftplugin/oil.lua
+++ after/ftplugin/oil.lua
@@ -1,10 +1,48 @@
 local oil = require("oil")
+local actions = require("oil.actions")
+local preview_shown = false
+
+local function close_preview()
+    actions.preview.callback()
+    preview_shown = false
+    vim.keymap.set("n", "<C-u>", "<C-u>zz", { buffer = true })
+    vim.keymap.set("n", "<C-d>", "<C-d>zz", { buffer = true })
+end
+
+local function open_preview()
+    actions.preview.callback()
+    preview_shown = true
+    vim.keymap.set("n", "<C-u>", actions.preview_scroll_up.callback, { buffer = true })
+    vim.keymap.set("n", "<C-d>", actions.preview_scroll_down.callback, { buffer = true })
+end
 
 vim.keymap.set(
     "n",
     "<Esc>",
     function()
-        oil.toggle_float()
+        if preview_shown then
+            close_preview()
+        else
+            -- close the floating window, if present
+            oil.toggle_float()
+        end
+    end,
+    {
+        desc = "Toggle oil float window",
+        silent = false,
+        buffer = true
+    }
+)
+
+vim.keymap.set(
+    "n",
+    "P",
+    function()
+        if preview_shown then
+            close_preview()
+        else
+            open_preview()
+        end
     end,
     {
         desc = "Toggle oil float window",
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -55,7 +55,8 @@ return {
                 -- ["<C-c>"] = "actions.close",
                 -- ["<Esc>"] = "toggle_float",
                 -- ["<C-c>"] = "toggle_float",
-                ["P"] = "actions.preview",
+                -- Preview action handling is in the ftplugin file
+                -- ["P"] = "actions.preview",
                 ["<C-l>"] = "actions.refresh",
                 ["-"] = "actions.parent",
                 ["_"] = "actions.open_cwd",
@@ -90,7 +91,7 @@ return {
             -- Configuration for the floating window in oil.open_float
             float = {
                 -- Padding around the floating window
-                padding = 2,
+                padding = 3,
                 max_width = 0,
                 max_height = 0,
                 border = "rounded",
@@ -108,7 +109,7 @@ return {
                 -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
                 -- min_width and max_width can be a single value or a list of mixed integer/float types.
                 -- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
-                max_width = 0.9,
+                max_width = {100, 0.8},
                 -- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
                 min_width = { 40, 0.4 },
                 -- optionally define an integer/float for the exact width of the preview window
@@ -116,7 +117,7 @@ return {
                 -- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
                 -- min_height and max_height can be a single value or a list of mixed integer/float types.
                 -- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
-                max_height = 0.9,
+                max_height = {80, 0.9},
                 -- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
                 min_height = { 5, 0.1 },
                 -- optionally define an integer/float for the exact height of the preview window
