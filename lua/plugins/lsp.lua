9ce02d2e23ee13e00970baa778a9018de7312507
noah lozevski <nlozevsk@amazon.com>
2023-11-21 09:49:40 -0800

    fixed the bad lock file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,15 +3,20 @@ return {
     cmd = 'LspInfo',
     -- TODO: figure out issues with lazy loading
     event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {{'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
-    {
-        'williamboman/mason.nvim',
-        build = function()
-            pcall(vim.cmd, 'MasonUpdate')
-        end
-    }, {'lukas-reineke/lsp-format.nvim'}, -- Required for async format on save
-    {'hrsh7th/cmp-nvim-lsp-signature-help'}, {'hrsh7th/cmp-nvim-lsp-document-symbol'}, {'neovim/nvim-lspconfig'} -- Required
+    dependencies = {
+        { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+        {
+            'williamboman/mason.nvim',
+            build = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end
+        },
+        { 'lukas-reineke/lsp-format.nvim' }, -- Required for async format on save
+        { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+        { 'hrsh7th/cmp-nvim-lsp-document-symbol' },
+        { 'neovim/nvim-lspconfig' } -- Required
+
     },
     config = function()
         -- load lsp config
---
+++ lua/plugins/xbase.lua
@@ -0,0 +1,67 @@
+-- for swift / xcode support
+return {
+    'xbase-lab/xbase',
+    build = "make install",
+    -- event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+        { "neovim/nvim-lspconfig" }
+    },
+    config = function()
+        require 'xbase'.setup({
+            --- Log level. Set it to ERROR to ignore everything
+            log_level = vim.log.levels.ERROR,
+            --- Options to be passed to lspconfig.nvim's sourcekit setup function.
+            --- Setting this to {} is sufficient, However, it is strongly recommended to use on_attach key to setup custom mappings
+            sourcekit = {
+                cmd = { "sourcekit-lsp", "--log-level", "error" },
+                filetypes = { "swift" },
+                root_dir = require("lspconfig").util.root_pattern(".git", "Package.swift", "project.yml", "Project.swift"),
+            },
+            --- Statusline provider configurations
+            statusline = {
+                watching = { icon = "", color = "#1abc9c" },
+                device_running = { icon = "", color = "#4a6edb" },
+                success = { icon = "", color = "#1abc9c" },
+                failure = { icon = "", color = "#db4b4b" },
+            },
+            --- Simulators to only include.
+            --- run `xcrun simctl list` to get a full list of available simulator
+            --- If the list is empty then all simulator available  will be included
+            simctl = {
+                iOS = {
+                    -- "iPhone 13 Pro", --- only use this devices
+                },
+                watchOS = {}, -- all available devices
+                tvOS = {},    -- all available devices
+            },
+            --- Log buffer configurations
+            log_buffer = {
+                --- Whether toggling the buffer should auto focus to it?
+                focus = true,
+                --- Split Log buffer height
+                height = 20,
+                --- Vsplit Log buffer width
+                width = 75,
+                --- Default log buffer direction: { "horizontal", "vertical" }
+                default_direction = "horizontal",
+            },
+            --- Mappings
+            mappings = {
+                --- Whether xbase mapping should be disabled.
+                enable = false,
+                --- Open build picker. showing targets and configuration.
+                build_picker = "<leader>b", --- set to 0 to disable
+                --- Open run picker. showing targets, devices and configuration
+                run_picker = "<leader>r",   --- set to 0 to disable
+                --- Open watch picker. showing run or build, targets, devices and configuration
+                watch_picker = "<leader>s", --- set to 0 to disable
+                --- A list of all the previous pickers
+                all_picker = "<leader>ef",  --- set to 0 to disable
+                --- horizontal toggle log buffer
+                toggle_split_log_buffer = "<leader>ls",
+                --- vertical toggle log buffer
+                toggle_vsplit_log_buffer = "<leader>lv",
+            },
+        }) -- see default configuration bellow
+    end
+}
