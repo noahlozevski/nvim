4c9d3e5b5448e65f3dddcbcd4a9707619857afb2
noah lozevski <noahlozevski@gmail.com>
2023-12-08 19:49:52 -0800

    remove cursor blinking

--- lua/lozevski/set.lua
+++ lua/lozevski/set.lua
@@ -1,5 +1,6 @@
 -- vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 -- vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.guicursor = "a:blinkon0"
 
 vim.opt.guicursor = ""
 vim.opt.nu = true
